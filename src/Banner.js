67315a3405925c4944659ee377cc3932a32d30df
Adil Alam <alamadil540@gmail.com>
2021-02-18 18:05:43 +0500

    styles

--- src/Banner.js
+++ src/Banner.js
@@ -20,6 +20,10 @@ function Banner() {
     fetchData();
   }, []);
 
+   function truncate(str, n) {
+     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+   }
+
   return (
     <header
       className="banner"
@@ -39,7 +43,7 @@ function Banner() {
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_description">{movie?.overview}</h1>
+        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
       <div className="banner--fadeBottom" />
     </header>
